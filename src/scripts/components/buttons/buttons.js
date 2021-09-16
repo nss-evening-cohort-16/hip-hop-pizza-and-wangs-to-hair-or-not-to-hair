5623a6a6d1d5e0492edec95089fac338ea6cad47
Joe Gibbs <joseph.gibbs1188@gmail.com>
2021-09-15 22:14:41 -0500

    Put together addItemForm.

--- src/scripts/components/buttons/buttons.js
+++ src/scripts/components/buttons/buttons.js
@@ -14,44 +14,35 @@ const viewRevenueButton = () => {
   document.querySelector('#store').innerHTML = domString;
 };
 
-// ITEM DETAILS PAGE
-const addItemButton = () => {
-  const domString = '<button id="add-item-button" class="btn btn-success">Add Item</button>';
-  document.querySelector('#store').innerHTML = domString;
-};
+// // ITEM DETAILS PAGE
+// const addItemButton = () => {
+//   const domString = '<button id="add-item-button" class="btn btn-success">Add Item</button>';
+//   document.querySelector('#store').innerHTML = domString;
+// };
 
-const goToPaymentButton = () => {
-  const domString = '<button id="go-to-payment-button" class="btn btn-info">Go To Payment</button>';
-  document.querySelector('#store').innerHTML = domString;
-};
+// const goToPaymentButton = () => {
+//   const domString = '<button id="go-to-payment-button" class="btn btn-info">Go To Payment</button>';
+//   document.querySelector('#store').innerHTML = domString;
+// };
 
-// SUBMIT ORDER PAGE
-// This is the button on page 5 of the figma prototype. I changed the name to Submit because Create Order was already used and Submit made more sense to me.
-const submitOrderButton = () => {
-  const domString = '<button id="submit-order-button" class="btn btn-success">Submit Order</button>';
-  document.querySelector('#store').innerHTML = domString;
-};
+// // SUBMIT ORDER PAGE
+// // This is the button on page 5 of the figma prototype. I changed the name to Submit because Create Order was already used and Submit made more sense to me.
+// const submitOrderButton = () => {
+//   const domString = '<button id="submit-order-button" class="btn btn-success">Submit Order</button>';
+//   document.querySelector('#store').innerHTML = domString;
+// };
 
-// SUBMIT ITEM PAGE
-// This is the button on page 6 of the figma prototype. I change the name to Submit because Create Item was already used and Submit made more sense to me.
-const submitItemButton = () => {
-  const domString = '<button id="submit-item-button" class="btn btn-success">Submit Item</button>';
-  document.querySelector('#store').innerHTML = domString;
-};
+// // SUBMIT ITEM PAGE
+// // This is the button on page 6 of the figma prototype. I change the name to Submit because Create Item was already used and Submit made more sense to me.
+// const submitItemButton = () => {
+//   const domString = '<button id="submit-item-button" class="btn btn-success">Submit Item</button>';
+//   document.querySelector('#store').innerHTML = domString;
+// };
 
-// CLOSE ORDER PAGE
-const closeOrderButton = () => {
-  const domString = '<button id="close-order-button" class="btn btn-success">Close Order</button>';
-  document.querySelector('#store').innerHTML = domString;
-};
+// // CLOSE ORDER PAGE
+// const closeOrderButton = () => {
+//   const domString = '<button id="close-order-button" class="btn btn-success">Close Order</button>';
+//   document.querySelector('#store').innerHTML = domString;
+// };
 
-export {
-  viewOrdersButton,
-  createOrderButton,
-  viewRevenueButton,
-  addItemButton,
-  goToPaymentButton,
-  submitOrderButton,
-  submitItemButton,
-  closeOrderButton
-};
+export { viewOrdersButton, createOrderButton, viewRevenueButton };
--- src/scripts/components/forms/addItemForm.js
+++ src/scripts/components/forms/addItemForm.js
@@ -1,3 +1,18 @@
 const addItemForm = () => {
-    
-}
+  document.querySelector('#form-container').innerHTML = `
+      <form id="add-item-form" class="mb-4">
+        <div class="form-group">
+          <label for="itemName">Item Name</label>
+          <input type="text" class="form-control" id="itemName" placeholder="Name" required>
+        </div>
+        <div class="form-group">
+          <label for="itemPrice">Item Price</label>
+          <input type="text" class="form-control" id="itemPrice" placeholder="Price" required>
+        </div>
+        <div class="form-button">
+            <button id="submit-item-button" class="btn btn-success">Submit Item</button>
+        </div>
+      </form>`;
+};
+
+export default addItemForm;
