be36ebdcd1862f10a85dbe793d620d4e935054b4
Joe Gibbs <joseph.gibbs1188@gmail.com>
2021-09-16 18:05:47 -0500

    refactored buttons to work the way they should

--- src/scripts/components/buttons/buttons.js
+++ src/scripts/components/buttons/buttons.js
@@ -1,48 +1,14 @@
 // BASIC MENU BUTTONS
-const viewOrdersButton = () => {
-  const domString = '<button id="view-orders-button" class="btn btn-success">View Orders</button>';
-  document.querySelector('#main-buttons').innerHTML = domString;
+const mainMenuButtons = () => {
+  document.querySelector('#store').innerHTML += '<div><button id="view-orders-button" class="btn btn-success">View Orders</button></div>';
+  document.querySelector('#store').innerHTML += '<div><button id="create-order-button" class="btn btn-info">Create an Order</button></div>';
+  document.querySelector('#store').innerHTML += '<div><button id="view-revenue-button" class="btn btn-danger">View Revenue</button></div>';
 };
 
-const createOrderButton = () => {
-  const domString = '<button id="create-order-button" class="btn btn-info">Create an Order</button>';
-  document.querySelector('#main-buttons').innerHTML = domString;
+// ITEM DETAILS PAGE BUTTONS
+const itemDetailsButtons = () => {
+  document.querySelector('#store').innerHTML += '<button id="add-item-button" class="btn btn-success">Add Item</button>';
+  document.querySelector('#store').innerHTML += '<button id="go-to-payment-button" class="btn btn-info">Go To Payment</button>';
 };
 
-const viewRevenueButton = () => {
-  const domString = '<button id="view-revenue-button" class="btn btn-danger">View Revenue</button>';
-  document.querySelector('#main-buttons').innerHTML = domString;
-};
-
-// // ITEM DETAILS PAGE
-// const addItemButton = () => {
-//   const domString = '<button id="add-item-button" class="btn btn-success">Add Item</button>';
-//   document.querySelector('#store').innerHTML = domString;
-// };
-
-// const goToPaymentButton = () => {
-//   const domString = '<button id="go-to-payment-button" class="btn btn-info">Go To Payment</button>';
-//   document.querySelector('#store').innerHTML = domString;
-// };
-
-// // SUBMIT ORDER PAGE
-// // This is the button on page 5 of the figma prototype. I changed the name to Submit because Create Order was already used and Submit made more sense to me.
-// const submitOrderButton = () => {
-//   const domString = '<button id="submit-order-button" class="btn btn-success">Submit Order</button>';
-//   document.querySelector('#store').innerHTML = domString;
-// };
-
-// // SUBMIT ITEM PAGE
-// // This is the button on page 6 of the figma prototype. I change the name to Submit because Create Item was already used and Submit made more sense to me.
-// const submitItemButton = () => {
-//   const domString = '<button id="submit-item-button" class="btn btn-success">Submit Item</button>';
-//   document.querySelector('#store').innerHTML = domString;
-// };
-
-// // CLOSE ORDER PAGE
-// const closeOrderButton = () => {
-//   const domString = '<button id="close-order-button" class="btn btn-success">Close Order</button>';
-//   document.querySelector('#store').innerHTML = domString;
-// };
-
-export { viewOrdersButton, createOrderButton, viewRevenueButton };
+export { mainMenuButtons, itemDetailsButtons };
