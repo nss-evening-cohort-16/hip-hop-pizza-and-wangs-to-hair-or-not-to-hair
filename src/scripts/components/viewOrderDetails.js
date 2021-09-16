24ac8096ff0a4498b5ead5d4d7ca0b720920661a
DerekMalone <laxmalone@gmail.com>
2021-09-16 17:51:49 -0500

    created card for items including buttons

---
+++ src/scripts/components/viewOrderDetails.js
@@ -0,0 +1,19 @@
+const showItems = (array) => {
+  array.forEach((obj) => {
+    document.querySelector('#').innerHTML += `
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-item-name">${obj.item_name}</h5>
+        <h4 class="card-item-price">${obj.item_price}</h4>
+        <hr>
+        <i class="fas fa-edit btn btn-info" id="update-item-btn"></i>
+        <i class="btn btn-danger fas fa-trash-alt" id="delete-item-btn"></i>
+        </div>
+    </div>
+  `;
+  });
+  document.querySelector('#').innerHTML = '<button class="btn btn-success btn-lg mb-4" id="add-item-btn">Add Another Item</button>';
+  document.querySelector('#').innerHTML = '<button class="btn btn-success btn-lg mb-4" id="go-to-payment-btn">Go To Payment</button>';
+};
+
+export default showItems;
