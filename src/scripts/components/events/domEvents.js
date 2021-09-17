9b9b8ce7fdef59cb0fc32445ac5f932014861569
Austin Parker <austincparker@gmail.com>
2021-09-16 19:33:48 -0500

    added form events to domevents

--- src/scripts/components/events/domEvents.js
+++ src/scripts/components/events/domEvents.js
@@ -2,4 +2,31 @@
 
 // Form Events
 
+const formEvents = () => {
+  document.querySelector('#main-container').addEventListener('click', (e) => {
+    // CREATE ORDER
+    if (e.target.id.includes('submit-order')) {
+      e.preventDefault();
+      const orderObj = {
+        customer_name: document.querySelector('#customer-name').value,
+        customer_phone: document.querySelector('#customer-phone').value,
+        customer_email: document.querySelector('#customer-email').value,
+        timestamp: Date.now()
+      };
+      console.warn(orderObj);
+    }
+    // CREATE ITEM
+    if (e.target.id.includes('submit-item-button')) {
+      e.preventDefault();
+      const itemObj = {
+        item_name: document.querySelector('#itemName').value,
+        item_price: document.querySelector('#itemPrice').value
+      };
+      console.warn(itemObj);
+    };
+  });
+};
+
 // Dom Events
+
+export default formEvents;
