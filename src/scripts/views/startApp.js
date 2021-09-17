b7d98a59c0db230cac01a7bb22d3119fb3c91a0c
Elle Lawrence <lawrence.danielle@gmail.com>
2021-09-16 19:39:13 -0500

    added domEvents to start App

--- src/scripts/views/startApp.js
+++ src/scripts/views/startApp.js
@@ -3,11 +3,13 @@ import logoutButton from '../components/buttons/logoutButton';
 import domBuilder from '../components/domBuilder';
 import navBar from '../components/navBar';
 import revenue from '../components/revenueView';
+import domEvents from '../components/events/domEvents';
 
 const startApp = (user) => {
   domBuilder();
   navBar();
   logoutButton();
+  domEvents();
   mainMenuBuilder(user);
   revenue();
   console.warn(user);
