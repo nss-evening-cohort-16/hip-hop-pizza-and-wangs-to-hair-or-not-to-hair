14a497d2d914f6042a8f62bb352fe685bc7fed57
Joe Gibbs <joseph.gibbs1188@gmail.com>
2021-09-20 20:38:44 -0500

    Testing out background image

--- src/scripts/views/homeLoggedOut.js
+++ src/scripts/views/homeLoggedOut.js
@@ -2,15 +2,15 @@ import logo from '../../assets/images/hhpw-record.png';
 import signIn from '../helpers/auth/signIn';
 
 const homeLoggedOut = () => {
-  const domString = '<div><button id="google-auth" class="btn btn-danger">LOGIN</button></div>';
-  const frontPageLogo = `
-    <div class="frontPage">
+  const domString = `
+  <div class ="frontPage">
+      <button id="google-auth" class="btn btn-danger">LOGIN</button></div>
+    <div>
       <img class ="frontPageLogo" src='${logo}' alt="Hip Hop Pizza Logo" width="400" height="400" class="d-inline-block align-text-top">
-      
     </div>
+  </div>
   `;
   document.querySelector('#app').innerHTML = domString;
-  document.querySelector('#app').innerHTML += frontPageLogo;
   document.querySelector('#google-auth').addEventListener('click', signIn);
 };
 
