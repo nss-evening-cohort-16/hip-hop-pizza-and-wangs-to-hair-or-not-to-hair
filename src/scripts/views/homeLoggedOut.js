70d50a3f68831ae9f96bb0c9ac58d7ff7d8fa736
Joe Gibbs <joseph.gibbs1188@gmail.com>
2021-09-20 22:02:52 -0500

    Merging dev

--- src/scripts/views/homeLoggedOut.js
+++ src/scripts/views/homeLoggedOut.js
@@ -6,10 +6,12 @@ const homeLoggedOut = () => {
   <div class ="frontPage">
       <button id="google-auth" class="btn btn-danger">LOGIN</button></div>
     <div>
-      <img class ="frontPageLogo" src='${logo}' alt="Hip Hop Pizza Logo" width="400" height="400" class="d-inline-block align-text-top">
+      <img class="frontPageLogo" src='${logo}' alt="Hip Hop Pizza Logo" width="400" height="400" class="d-inline-block align-text-top">
+
     </div>
   </div>
   `;
+
   document.querySelector('#app').innerHTML = domString;
   document.querySelector('#google-auth').addEventListener('click', signIn);
 };
