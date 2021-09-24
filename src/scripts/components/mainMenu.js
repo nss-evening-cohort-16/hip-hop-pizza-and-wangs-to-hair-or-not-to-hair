2cd449b62f675caceb626c363e5679db9ec7c641
DerekMalone <laxmalone@gmail.com>
2021-09-23 21:49:26 -0500

    styled main pages buttons

--- src/scripts/components/mainMenu.js
+++ src/scripts/components/mainMenu.js
@@ -5,9 +5,9 @@ const mainMenuBuilder = (user) => {
   clearDom();
 
   document.querySelector('#form-container').innerHTML += `<h1 class="greeting">Welcome, ${user.displayName}</h1>`;
-  document.querySelector('#store').innerHTML += '<div><button id="view-orders-button" class="btn btn-success">View Orders</button></div>';
-  document.querySelector('#store').innerHTML += '<div><button id="create-order-button" class="btn btn-info">Create an Order</button></div>';
-  document.querySelector('#store').innerHTML += '<div><button id="view-revenue-button" class="btn btn-danger">View Revenue</button></div>';
+  document.querySelector('#store').innerHTML += '<div><button id="view-orders-button" class="btn main-menu-btn btn-success">View Orders</button></div>';
+  document.querySelector('#store').innerHTML += '<div><button id="create-order-button" class="btn main-menu-btn btn-info">Create an Order</button></div>';
+  document.querySelector('#store').innerHTML += '<div><button id="view-revenue-button" class="btn main-menu-btn btn-danger">View Revenue</button></div>';
 };
 
 export default mainMenuBuilder;
--- src/scripts/components/viewOrderDetails.js
+++ src/scripts/components/viewOrderDetails.js
@@ -6,9 +6,9 @@ const showItems = (orderFireKey, array) => {
   array.forEach((obj) => {
     document.querySelector('#store').innerHTML += `
     <div class="card" style="width: 18rem;">
-      <div class="card-body">
+      <div class="card-body item-style">
         <h5 class="card-item-name">${obj.item_name}</h5>
-        <h4 class="card-item-price">${obj.item_price}</h4>
+        <h4 class="card-item-price">$${obj.item_price}</h4>
         <hr>
         <i class="fas fa-edit btn btn-info" id="edit-item-btn--${obj.firebaseKey}--${orderFireKey}"></i>
         <i class="btn btn-danger fas fa-trash-alt" id="delete-item-btn--${obj.firebaseKey}--${orderFireKey}"></i>
