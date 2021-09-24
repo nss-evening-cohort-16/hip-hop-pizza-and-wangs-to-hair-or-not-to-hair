fef1f9bb2b894f4cb264e4c77504595d4dc8d4ae
Austin Parker <austincparker@gmail.com>
2021-09-24 17:15:26 -0500

    added class names to revenueView

--- src/scripts/components/revenueView.js
+++ src/scripts/components/revenueView.js
@@ -6,24 +6,24 @@ const revenue = (array) => {
 
   clearDom();
   document.querySelector('#store').innerHTML = `
-    <div class="card">
+    <div class="card rev-card">
   <div class="card-body">
-    <h2 class="card-title">REVENUE</h2>
-    <h4 class="card-title">TOTAL REVENUE: $${array.item_total} </h4>
-    <hr>
-    <p class="card-text">DATE RANGE: ${`${firstOrderDate} - ${lastOrderDate}`} </p>
-    <hr>
-    <p class="card-text">TOTAL TIPS: $${array.tip_total}</p>
-    <hr>
-    <p class="card-text">TOTAL CALL IN ORDERS: ${array.total_call_ins}</p>
-    <p class="card-text">TOTAL WALK IN ORDERS: ${array.total_walk_ins}</p>
-    <hr>
-    <p class="card-text">PAYMENT TYPES: </p>
-    <p class="card-text">CASH: ${array.cash_payment}</p>
-    <p class="card-text">CHECK: ${array.check_payment}</p>
-    <p class="card-text">DEBIT: ${array.debit_payment}</p>
-    <p class="card-text">CREDIT: ${array.credit_payment}</p>
-    <p class="card-text">MOBILE: ${array.mobile_payment}</p>
+    <h2 class="card-title rev-title">REVENUE</h2>
+    <h4 class="card-title rev-total">TOTAL REVENUE: $${array.item_total} </h4>
+    <p class="card-text rev-range">DATE RANGE: ${`${firstOrderDate} - ${lastOrderDate}`} </p>
+    <p class="card-text rev-tips">TOTAL TIPS: $${array.tip_total}</p>
+    <div class="rev-orders">
+      <p class="card-text">TOTAL CALL IN ORDERS: ${array.total_call_ins}</p>
+      <p class="card-text">TOTAL WALK IN ORDERS: ${array.total_walk_ins}</p>
+    </div>
+    <div class="rev-payment">
+      <p class="card-text">PAYMENT TYPES: </p>
+      <p class="card-text">CASH: ${array.cash_payment}</p>
+      <p class="card-text">CHECK: ${array.check_payment}</p>
+      <p class="card-text">DEBIT: ${array.debit_payment}</p>
+      <p class="card-text">CREDIT: ${array.credit_payment}</p>
+      <p class="card-text">MOBILE: ${array.mobile_payment}</p>
+    </div>
   </div>
 </div>`;
 };
