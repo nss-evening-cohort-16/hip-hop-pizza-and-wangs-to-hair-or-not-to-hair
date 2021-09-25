33987d787d44f13969e865219b016675b7b17b23
Elle Lawrence <lawrence.danielle@gmail.com>
2021-09-25 10:40:01 -0500

    revenue total complete

---
+++ src/scripts/helpers/data/getClosedOrders.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import firebaseConfig from '../../../api/apiKeys';
+
+const dbUrl = firebaseConfig.databaseURL;
+
+// PROMISE PULLING CLOSED ORDERS
+const getClosedOrders = () => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/orders.json?orderBy="order_closed"&equalTo=true`)
+    .then((response) => resolve(Object.values(response.data)))
+    .catch(reject);
+});
+
+export default getClosedOrders;
